3ed7059b99a569d7da35e380c5abe757ca1f6300
genshin impact <87218053+Raze9@users.noreply.github.com>
2023-03-15 18:40:55 +0800

    Add files via upload

---
+++ ui(front)/src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import router from './routes'
+import store from './vuex/store'
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(router)
+//挂载vuex
+app.use(store)
+
+
+app.mount('#app')
---
+++ ui(front)/src/views/LoveFlower.vue
@@ -0,0 +1,6 @@
+<template>
+   <FlowerList :type=1 image="/images/theme/flower_bannerLove_190906.jpg" />
+</template>
+ <script setup> 
+ import FlowerList from '../components/FlowerList.vue'
+ </script> 
