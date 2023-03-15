e416dd7fc43772a6dcedcc456622823d108d8708
genshin impact <87218053+Raze9@users.noreply.github.com>
2023-03-15 18:07:53 +0800

    Add files via upload

---
+++ flower.api/Controllers/ImageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MdFlower.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpGet]
+        public List<ImageModel> GetImages()
+        {
+            List<ImageModel> list = new List<ImageModel>()
+            {
+                new ImageModel() {ImageUrl="",CourseUrl=""}
+            };
+            return list;
+        }
+    }
+}
