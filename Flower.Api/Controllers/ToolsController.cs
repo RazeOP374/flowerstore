76c2b8084c1a12b69dc785aa2651a3a6dbba4588
genshin impact <87218053+Raze9@users.noreply.github.com>
2023-03-15 18:12:45 +0800

    Add files via upload

---
+++ Flower.Api/Controllers/ToolsController.cs
@@ -0,0 +1,17 @@
+﻿using MdFlower.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MdFlower.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ToolsController : ControllerBase
+    {
+        [HttpGet]
+        public void InitDatabase()
+        {
+            DbContext.InitDataBase();
+        }
+    }
+}
---
+++ Flower.Api/ImageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MdFlower.Api
+{
+    public class ImageModel
+    {
+        public string ImageUrl { get; set; }
+
+        public string CourseUrl { get; set; }
+    }
+}
